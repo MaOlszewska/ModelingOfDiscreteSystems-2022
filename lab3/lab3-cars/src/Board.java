79714065e9e65a179b331568a0372f04cc66cbb7
MaOlszewska <marto12@wp.pl>
2022-03-30 12:47:02 +0200

    lab4 done

--- lab3/lab3-cars/src/Board.java
+++ lab3/lab3-cars/src/Board.java
@@ -10,7 +10,7 @@ import java.awt.event.MouseEvent;
 public class Board extends JComponent implements MouseInputListener, ComponentListener {
     private static final long serialVersionUID = 1L;
     private Point[][] points;
-    private int size = 10;
+    private int size = 25;
     public int editType = 0;
 
     public Board(int length, int height) {
@@ -29,13 +29,85 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
                 points[x][y] = new Point();
             }
         }
+
+        for (int x = 0; x < points.length; ++x)
+            for (int y = 0; y < points[x].length; ++y)
+                if(y != points[x].length / 2 - 1 && y != points[x].length / 2 ){
+                    points[x][y].type = 5;
+                }
+                else{
+                    points[x][y].type = 0;
+                }
+
         // TODO
         for (int x = 0; x < points.length; ++x) {
-            for (int y = 0; y < points[x].length; ++y) {
-                if(x + 1 == points.length){points[x][y].next = points[0][y];}
-                else points[x][y].next = points[x + 1][y];
+            for (int y = 2; y < 4; ++y) {
+                if (y == 2) points[x][y].right = points[x][y+1];
+                if (y == 3) points[x][y].left = points[x][y-1];
+                points[x][y].next = points[((x + 1) % points.length)][y];
             }
         }
+
+    }
+
+    public boolean canOverTaking(int x, int y){
+        int maxSpeed = 2 * points[x][y].type + 1;
+        int distance1 = 7 ; // dystans z tyłu na prawym pasie
+        int distance2 = 7 ; // dystans z tyłu na lewym pasie
+        int distance3 = 7 ; // dystans z prozdu na lewym pasie
+
+        for(int i = 1; i <= 7 ; i++) {
+            if (points[(points.length + x - i) % points.length][3].type != 0) {
+                distance1 = i;
+                break;
+            }
+        }
+        for(int i = 1; i <= 7 ; i++) {
+            if (points[(points.length + x - i) % points.length][2].type != 0) {
+                distance2 = i;
+                break;
+            }
+        }
+        for(int i = 1; i <= 7 ; i++) {
+            if (points[(points.length + x + i) % points.length][2].type != 0) {
+                distance3 = i;
+                break;
+            }
+        }
+
+
+
+        if(points[x][y].speed < maxSpeed && distance1 >= 7 && distance2 >= 7 && distance3 >= points[x][y].speed ){return true;}
+        else{return false;}
+    }
+
+    public boolean canBack(int x, int y){
+        int maxSpeed = 2 * points[x][y].type + 1;
+        int distance1 = 7 ; // dystans z tyłu na prawym pasie
+        int distance2 = 7 ; // dystans z tyłu na lewym pasie
+        int distance3 = 7 ; // dystans z prozdu na lewym pasie
+
+        for(int i = 1; i <= 7; i++) {
+            if (points[(points.length + x - i) % points.length][3].type != 0) {
+                distance1 = i;
+                break;
+            }
+        }
+        for(int i = 1; i <= 7; i++) {
+            if (points[(points.length + x - i) % points.length][2].type != 0) {
+                distance2 = i;
+                break;
+            }
+        }
+        for(int  i = 1; i <= 7; i++) {
+            if (points[(points.length + x + i) % points.length][3].type != 0) {
+                distance3 = i;
+                break;
+            }
+        }
+
+        if(distance1 >= 7 && distance2 >= 7 && distance3 >= points[x][y].speed){return true;}
+        else{return false;}
     }
 
     public void iteration() {
@@ -45,10 +117,31 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
                 points[x][y].moved = false;
             }
         }
+
         for (int x = 0; x < points.length; ++x) {
-            for (int y = 0; y < points[x].length; ++y) {
-                // TODO
-                points[x][y].move();
+            for (int y = 2; y <= 3; ++y) {
+                if (points[x][y].type == 1 || points[x][y].type == 2 || points[x][y].type == 3  ) {
+                    if(y == 2 && canBack(x,y) && points[x][y].right.type == 0){
+                        points[x][y].back();
+                    }
+                }
+            }
+        }
+        for (int x = 0; x < points.length; ++x) {
+            for (int y = 2; y <= 3; ++y) {
+                if (points[x][y].type == 1 || points[x][y].type == 2 || points[x][y].type == 3 && !points[x][y].moved) {
+                    if(y == 3 && canOverTaking(x,y) && points[x][y].left.type == 0 ){
+                        points[x][y].overtaking();
+                    }
+                }
+            }
+        }
+
+        for (int x = 0; x < points.length; ++x) {
+            for (int y = 2; y <= 3; ++y) {
+                if (points[x][y].type == 1 || points[x][y].type == 2 || points[x][y].type == 3  ) {
+                    points[x][y].move();
+                }
             }
         }
         this.repaint();
@@ -57,7 +150,7 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
     public void clear() {
         for (int x = 0; x < points.length; ++x)
             for (int y = 0; y < points[x].length; ++y) {
-                points[x][y].clear();
+                if(points[x][y].type != 5) points[x][y].clear();
             }
         this.repaint();
     }
@@ -97,11 +190,21 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
 
                 // TODO
                 if(points[x][y].type == 1){
-                    g.setColor(new Color(0, 0, 0, 0.7f));
+                    g.setColor(new Color(1, 1, 0, 0.7f));
                 }
-                else{
+                else if(points[x][y].type == 0 ){
                     g.setColor(new Color(1, 1, 1, 0.7f));
                 }
+                else if(points[x][y].type == 2){
+                    g.setColor(new Color(0, 0, 1, 0.7f));
+                }
+                else if(points[x][y].type == 3 ){
+                    g.setColor(new Color(1, 0, 0, 0.7f));
+                }
+                else{
+                    g.setColor(new Color(0, 1, 0, 0.7f));
+                }
+
 
                 g.fillRect((x * size) + 1, (y * size) + 1, (size - 1), (size - 1));
             }
@@ -113,8 +216,11 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
         int x = e.getX() / size;
         int y = e.getY() / size;
         if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
-            if (editType == 0) {
+            if(editType==0){
                 points[x][y].clicked();
+            }
+            else {
+                points[x][y].type= editType;
             }
             this.repaint();
         }
@@ -130,8 +236,11 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
         int x = e.getX() / size;
         int y = e.getY() / size;
         if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
-            if (editType == 0) {
+            if(editType==0){
                 points[x][y].clicked();
+            }
+            else {
+                points[x][y].type= editType;
             }
             this.repaint();
         }
